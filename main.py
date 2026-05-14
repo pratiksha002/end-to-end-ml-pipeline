2300050cc3a5d85152974982d2342c5403805721
pratiksha002 <020606pratiksha@gmail.com>
2026-05-14 17:20:46 +0530

    Tested data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_cleaning import DataCleaning
+from src.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
 
@@ -13,4 +14,7 @@ if __name__ == "__main__":
     cleaning = DataCleaning()
     cleaned_df = cleaning.clean_data(df)
 
-    print(cleaned_df.head())
+    transformation = DataTransformation()
+    transformed_data = (transformation.transform_data(cleaned_df))
+
+    print(transformed_data)
